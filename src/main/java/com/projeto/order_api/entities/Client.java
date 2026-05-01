686a1ded2d3f637e19b64518a03807871c06690a
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2026-05-01 11:08:45 -0300

    add - class - Client

--- src/main/java/com/projeto/order_api/entities/Client.java
+++ src/main/java/com/projeto/order_api/entities/Client.java
@@ -1,5 +1,40 @@
 package com.projeto.order_api.entities;
 
+import java.time.LocalDate;
+
 public class Client {
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+
+    public Client(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 
 }
