4f97c57e24977cb38d5c23302751bf24c84f8fe5
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2026-05-01 10:41:31 -0300

    add - class - OrderItem

--- src/main/java/com/projeto/order_api/entities/OrderItem.java
+++ src/main/java/com/projeto/order_api/entities/OrderItem.java
@@ -1,5 +1,41 @@
 package com.projeto.order_api.entities;
 
 public class OrderItem {
+    private Integer quantity;
+    private Double price;
+    private Product product;
 
+    public OrderItem(Integer quantity, Double price, Product product) {
+        this.quantity = quantity;
+        this.price = price;
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Double subTotal() {
+        return quantity * price;
+    }
 }
