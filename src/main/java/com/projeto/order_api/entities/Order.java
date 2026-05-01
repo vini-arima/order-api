e26f03a7ed7209bbaa4bfd4e8d187c06f4e0b415
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2026-05-01 11:06:55 -0300

    add - class - Order

--- src/main/java/com/projeto/order_api/entities/Order.java
+++ src/main/java/com/projeto/order_api/entities/Order.java
@@ -1,5 +1,69 @@
 package com.projeto.order_api.entities;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private LocalDateTime moment;
+    private OrderStatus status;
+    private Client client;
+    // LISTA DE ITEMS
+    private List<OrderItem> items = new ArrayList<>();
+
+    public Order(LocalDateTime moment, OrderStatus status, Client client) {
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public LocalDateTime getMoment() {
+        return moment;
+    }
+
+    public void setMoment(LocalDateTime moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+    }
+
+    public void removeItem(OrderItem item) {
+        items.remove(item);
+    }
+
+    // FUNÇAO TOTAL
+    public Double total() {
+        double sum = 0.0;
+        for (OrderItem item : items) {
+            sum += item.subTotal();
+        }
+        return sum;
+    }
 
 }
